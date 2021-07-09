7ce0f54dd633b6730a2d5ac16f33a5534e0c3e79
Brett Spokes <brett.spokes@revature.net>
2021-07-09 09:17:19 -0700

    Fully functional gameplay, lacks simon circular styling

--- force-app/main/default/aura/SimonSays/SimonSaysController.js
+++ force-app/main/default/aura/SimonSays/SimonSaysController.js
@@ -1,31 +1,37 @@
 ({	
-    
+
     Init : function(component, event, helper){
         helper.Init(component,event);      
     },
     
     handleClick_yellow : function(component, event, helper) {
-        // yellow = 1			
+        // yellow = 1
+		if(component.get("v.inputWaitTimer")); else			
         helper.handleClick_yellow(component,event);         
     },
     
     handleClick_red : function(component, event, helper) {
-        // red = 2  	  
+        // red = 2
+		if(component.get("v.inputWaitTimer")); else  	  
         helper.handleClick_red(component,event);
     },
     
     handleClick_blue : function(component, event, helper) {
         // blue = 3       
-        helper.handleClick_blue(component,event);
+        if(component.get("v.inputWaitTimer")); else
+		helper.handleClick_blue(component,event);
     },
     
     handleClick_green : function(component, event, helper) {
         // green = 4        
-        helper.handleClick_green(component,event);
+		
+		if(component.get("v.inputWaitTimer")); else 
+		helper.handleClick_green(component,event);
+        
     },
 
     showDebugClick : function(component,event,helper) {
-        helper.showDebugClick(component,event);
+        helper.showDebugClick(component,event);		
     },
     
     
--- force-app/main/default/aura/SimonSays/SimonSaysHelper.js
+++ force-app/main/default/aura/SimonSays/SimonSaysHelper.js
@@ -1,5 +1,5 @@
 ({
-	//Called after a sequence is completed to add a new number onto it
+    //Called after a sequence is completed to add a new number onto it
     playSimon : function(component, event) {
         let sequenceString;
         //Random Number Generator
@@ -17,12 +17,12 @@
         sequenceString = "" + component.get("v.simonSequence");
         component.set("v.showSequence", sequenceString);
         
-		//calls a function to start parsing through the color sequence array 
-		this.parseColors(component, event);
+        //calls a function to start parsing through the color sequence array 
+        this.parseColors(component, event);
         
     },
     
-	//Get the inital number and allow player input
+    //Get the inital number and allow player input
     Init : function(component, event){
         let sequenceString;
         //Random Number Generator
@@ -30,24 +30,30 @@
             let num = Math.random() * (max - min) + min;
             return Math.round(num);
         };
-
-		//Adding on the random number
+        
+        //Adding on the random number
         component.set("v.simonSequence", [random(1,4)]);
         sequenceString = "" + component.get("v.simonSequence");
         component.set("v.showSequence", sequenceString);
         component.set("v.inputWaitTimer", false);
         
-		this.parseColors(component, event);
+        document.getElementById("display-yellow").style.color="gold";
+        document.getElementById("display-red").style.color="red";
+        document.getElementById("display-blue").style.color="blue";
+        document.getElementById("display-green").style.color="green";
+        
+        this.parseColors(component, event);
+        
     },
-      
+    
     //The set of the following color related methods are to check player input against an array of the color sequence
     handleClick_yellow : function(component, event) {
         // yellow = 1			
-        
+        this.playSound(1);    
         if (component.get("v.simonSequence")[component.get("v.progressPosition")] == 1 /* Yellow*/)
         {
-			// if it is the last number in the sequence, call playSimon and add a new number onto the end, and return player to the 
-			//first position of the sequence
+            // if it is the last number in the sequence, call playSimon and add a new number onto the end, and return player to the 
+            //first position of the sequence
             if(component.get("v.simonSequence").length == component.get("v.progressPosition")+1){
                 this.playSimon(component, event);
                 component.set("v.progressPosition",0);
@@ -57,10 +63,13 @@
             }
         }
         else {
-			// If incorrect input is given turn game off
+            // If incorrect input is given turn game off
             component.set("v.progressPosition", 0);
             alert("Incorrect Input");
-            document.getElementById("display").style.color="black";
+            document.getElementById("display-yellow").style.color="black";
+            document.getElementById("display-red").style.color="black";
+            document.getElementById("display-blue").style.color="black";
+            document.getElementById("display-green").style.color="black";
             component.set("v.inputWaitTimer", true);
         }
         
@@ -69,7 +78,7 @@
     
     handleClick_red : function(component, event) {
         // red = 2  	
-        
+        this.playSound(2);    
         if (component.get("v.simonSequence")[component.get("v.progressPosition")] == 2 /* Red */)
         {
             if(component.get("v.simonSequence").length == component.get("v.progressPosition")+1){
@@ -84,14 +93,17 @@
         else {
             component.set("v.progressPosition", 0);
             alert("Incorrect Input");
-            document.getElementById("display").style.color="black";
+            document.getElementById("display-yellow").style.color="black";
+            document.getElementById("display-red").style.color="black";
+            document.getElementById("display-blue").style.color="black";
+            document.getElementById("display-green").style.color="black";
             component.set("v.inputWaitTimer", true);
         }
     },
     
     handleClick_blue : function(component, event) {
         // blue = 3      
-        
+        this.playSound(3);    
         if (component.get("v.simonSequence")[component.get("v.progressPosition")] == 3 /* Blue */)
         {
             if(component.get("v.simonSequence").length == component.get("v.progressPosition")+1){
@@ -107,13 +119,17 @@
         else {
             component.set("v.progressPosition", 0);
             alert("Incorrect Input");
-            document.getElementById("display").style.color="black";
+            document.getElementById("display-yellow").style.color="black";
+            document.getElementById("display-red").style.color="black";
+            document.getElementById("display-blue").style.color="black";
+            document.getElementById("display-green").style.color="black";
             component.set("v.inputWaitTimer", true);
         }
     },
     
     handleClick_green : function(component, event) {
-        // green = 4        
+        // green = 4
+		this.playSound(4);       
         if (component.get("v.simonSequence")[component.get("v.progressPosition")] == 4 /* Green */)
         {
             if(component.get("v.simonSequence").length == component.get("v.progressPosition")+1){
@@ -128,12 +144,15 @@
         else {
             component.set("v.progressPosition", 0);
             alert("Incorrect Input");
-            document.getElementById("display").style.color="black";
+            document.getElementById("display-yellow").style.color="black";
+            document.getElementById("display-red").style.color="black";
+            document.getElementById("display-blue").style.color="black";
+            document.getElementById("display-green").style.color="black";
             component.set("v.inputWaitTimer", true);
         }
     },
     
-	//For showing a debug section
+    //For showing a debug section
     showDebugClick : function(component,event) {
         if (component.get("v.showDebug"))
         {
@@ -145,76 +164,115 @@
         }
     },
     
-	//purpose is to set up the parsing of the color sequence and pass it to a function that has a timer functionality
+    //purpose is to set up the parsing of the color sequence and pass it to a function that has a timer functionality
     parseColors : function(component, event) {
         component.set("v.inputWaitTimer",true);
-		
-		var sequence = component.get("v.simonSequence").map((x) => x);
-		sequence.reverse();       
+        
+        var sequence = component.get("v.simonSequence").map((x) => x);
+        sequence.reverse();       
         var parserLength = sequence.length; 
         var context = this;
         this.colorPreview(component , event , parserLength);     
         this.parseColorsRepeater(context, parserLength, component, sequence);
     },
-
-	//purpose is to utilize the Window setTimeout() method to give time for the program to be able to show the sequence, calling itself
-	//as it parses through a reverse of the sequence
+    
+    //purpose is to utilize the Window setTimeout() method to give time for the program to be able to show the sequence, calling itself
+    //as it parses through a reverse of the sequence
     parseColorsRepeater : function(helper, parser, component, sequence){
         window.setTimeout(function(){
             parser--;
             if(parser>=0){
                 helper.parseColorsBlack(helper,parser, component, sequence);
-                
+                var urlSound = $A.get('$Resource.Beep_C');
                 
                 if(sequence[parser] == 1){
-                    document.getElementById("display").style.color="gold";
-
+                    document.getElementById("display-yellow").style.color="PaleGoldenRod";
+                    urlSound = $A.get('$Resource.Beep_C');
+                    
                 }
                 else if(sequence[parser] == 2){
-                    document.getElementById("display").style.color="red";
-
+                    document.getElementById("display-red").style.color="Tomato";
+                    urlSound = $A.get('$Resource.Beep_D');
                 }
                     else if(sequence[parser] == 3){
-                        document.getElementById("display").style.color="blue";
-
+                        document.getElementById("display-blue").style.color="DeepSkyBlue";
+                        urlSound = $A.get('$Resource.Beep_F');
                     }
                         else if(sequence[parser] == 4){
-                            document.getElementById("display").style.color="green";
-                        }  
+                            document.getElementById("display-green").style.color="PaleGreen";
+                            urlSound = $A.get('$Resource.Beep_G');
+                        }
+                
+                var beepsound = new Audio(urlSound);   
+                beepsound.play();	  
             }
-			else
+            else
             {
-				document.getElementById("display").style.color="black";
+                document.getElementById("display-yellow").style.color="gold";
+                document.getElementById("display-red").style.color="red";
+                document.getElementById("display-blue").style.color="blue";
+                document.getElementById("display-green").style.color="green";
                 component.set("v.inputWaitTimer",false);   
             }
-        },750);
-    },
-
-    //purpose is to add a spacer of black between showing the sequence's colors, so that if there are repeats it's easy to distinguish
-	parseColorsBlack : function(helper, parser, component, sequence){
-        window.setTimeout(function(){
-                helper.parseColorsRepeater(helper,parser, component, sequence);
-                    document.getElementById("display").style.color="black";
-                        
+            
+            
+            
         },750);
     },
     
-	//purpose is to parse through the four colors and set the prompter.
-	colorPreview : function (component, event, input){
+    //purpose is to add a spacer of black between showing the sequence's colors, so that if there are repeats it's easy to distinguish
+    parseColorsBlack : function(helper, parser, component, sequence){
+        window.setTimeout(function(){
+            helper.parseColorsRepeater(helper,parser, component, sequence);
+            document.getElementById("display-yellow").style.color="gold";
+            document.getElementById("display-red").style.color="red";
+            document.getElementById("display-blue").style.color="blue";
+            document.getElementById("display-green").style.color="green";
+            
+        },750);
+    },
+    
+    //purpose is to parse through the four colors and set the prompter.
+    colorPreview : function (component, event, input){
         let i = input;
-
+        
         if(component.get("v.simonSequence")[i] == 1){
-            document.getElementById("display").style.color="gold";
+            document.getElementById("display-yellow").style.color="PaleGoldenRod";
         }
         else if(component.get("v.simonSequence")[i] == 2){
-            document.getElementById("display").style.color="red";
+            document.getElementById("display-red").style.color="Tomato";
         }
             else if(component.get("v.simonSequence")[i] == 3){
-                document.getElementById("display").style.color="blue";
+                document.getElementById("display-blue").style.color="DeepSkyBlue";
             }
                 else if(component.get("v.simonSequence")[i] == 4){
-                    document.getElementById("display").style.color="green";
+                    document.getElementById("display-green").style.color="PaleGreen";
                 }  
     },
+    
+    //Moved inside of the repeater so it has it's functionality
+	//Purpose to get an input and play back the appropriate note
+    playSound : function (player_input) {
+        var urlSound = $A.get('$Resource.BeepNoise');
+        
+        switch(player_input)
+        {
+            case 1:	urlSound = $A.get('$Resource.Beep_C');
+                break;
+                
+            case 2:	urlSound = $A.get('$Resource.Beep_D');
+                break;
+                
+            case 3:	urlSound = $A.get('$Resource.Beep_F');
+                break;
+                
+            case 4:	urlSound = $A.get('$Resource.Beep_G');
+                break;
+        }
+        
+        
+        var beepsound = new Audio(urlSound);   
+        beepsound.play();
+    }
     
 })
